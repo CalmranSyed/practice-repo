259cfdace4bc69ee6340af37658f065f0e84b90f
CalmranSyed <68997464+CalmranSyed@users.noreply.github.com>
2021-11-20 16:08:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-// let url = "api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=99cdda2906b9a8ab0c2ddd20f791de7c";
-// let responsetxt= req.responseText;
-// let state= req.readyState;
-// let reqstatus = req.statusText;
-// let headers = req.getAllResponseHeaders;
 let req = new XMLHttpRequest();
-req.open("GET", "api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=99cdda2906b9a8ab0c2ddd20f791de7c" );
-if (req.readyState == 4) {
-    console.log(req.readyState , req.statusText);
-    console.log(headers);
+req.open("GET", "jsonplaceholder.typicode.com/users" );
+req.responseType = "json";
+req.setRequestHeader("Access-Control-Allow-Origin", "*");
+req.setRequestHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+req.onreadystatechange = function () {
+    if (req.readyState === 4) {
+        console.log(req.statusText , req.status);
+        console.log(req.response);
+    }
 }
-console.log("Bad request", req.ser , req.statusText);
-console.log(headers);
 req.onerror = function() {
-    console.log("oh no");
+    console.log("Error");
 }
 req.send();
